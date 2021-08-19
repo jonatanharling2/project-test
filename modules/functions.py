910f33f9aee9a64e3d7f3267120929ea98396df3
jonatanharling2 <jonatan.harling@fcg.se>
2021-08-19 12:58:49 +0200

    DOCS: Added docstring to add_ten function.

--- modules/functions.py
+++ modules/functions.py
@@ -1,5 +1,24 @@
+"""
+=========
+Functions
+=========
+Where we keep the useful functions..
+"""
+
+
 def add_ten(param):
-    """Added docstring..."""
+    """
+    Function adds ten to the given input parameters:
+
+    Args:
+        param(float): The input number to add 10 to.
+
+    Returns:
+        float: The output number
+
+    Notes:
+        This is one of the greatest functions ever written.
+    """
     return param + 10
 
 
